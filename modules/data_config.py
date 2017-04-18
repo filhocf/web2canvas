3e4ed49b7437fcc4ae1b19e3d83bcbec91052419
Douglas Soares de Andrade <contato@douglasandrade.com>
2017-04-18 15:52:01 -0300

    Atualiza as configuracoes
    - Remove as configuracoes do Facebook
    - Adiciona configuracoes do LDAP

--- modules/data_config.py
+++ modules/data_config.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 
-## Autenticação com Facebook
-
-FACEBOOK_ID = ''
-FACEBOOK_SECRET = ''
-
 ## Autenticação para envio de email
 
 EMAIL_SERVER = 'smtp.gmail.com:587'
 CLIENT_EMAIL = 'you@gmail.com'
 CLIENT_LOGIN = 'username:password'
+
+## Configuracao do LDAP
+LDAP_CONFIG = {
+    "mode": 'custom',
+    "server": 'ldap.dataprev.gov.br',
+    "base_dn": 'dc=gov,dc=br',
+    "username_attrib": 'uid',
+    "manage_user": True,
+    "manage_groups": False,
+    "custom_scope": 'subtree',
+    "self_signed_certificate": True
+}
